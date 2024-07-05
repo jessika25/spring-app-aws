e2b9d3dc86d4dcdf9c2f5048d6de567ca29b5bc8
Jessika dos Santos Nepomuceno <jessikas@br.ibm.com>
2024-07-04 22:53:57 -0300

    sending mvn app

---
+++ src/main/java/com/example/spring_app_aws/SpringAppAwsApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_app_aws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAppAwsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAppAwsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/spring_app_aws/SpringAppAwsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_app_aws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAppAwsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
